b33a9a45b06cc6f990f294e0aafbb1425eb02294
soshwosh <sasharavpersaud@gmail.com>
2025-02-01 11:42:49 -0500

    displayed CSV data to UI

--- job_board/src/ui/JobList.tsx
+++ job_board/src/ui/JobList.tsx
@@ -1,24 +1,54 @@
 "use client";
 import Papa from "papaparse";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+
+// type Job = { [key: string]: string };
+type Job = {
+  jobTitle: string;
+  companyName: string;
+  location: string;
+  description: string;
+  requirements: string;
+};
 
 export default function JobList() {
+  const [data, setData] = useState<Job[]>([]);
+
   useEffect(() => {
     fetch("/data/jobs.csv")
       .then((response) => response.text())
       .then((csvText) => {
-        Papa.parse(csvText, {
+        Papa.parse<Record<string, string>>(csvText, {
           header: true,
-          complete: function (results) {
-            console.log(results.data);
+          skipEmptyLines: true,
+          complete: (result) => {
+            console.log(result.data);
+            const parsedData = result.data.map((row) => ({
+              jobTitle: row["Job Title"] || "",
+              companyName: row["Company Name"] || "",
+              location: row["Location"] || "",
+              description: row["Job Description"] || "",
+              requirements: row["Requirements"] || "",
+            })) as Job[];
+            setData(parsedData);
           },
         });
       });
-  });
+  }, []);
+
+  console.log(data[0]);
 
   return (
     <div>
-      <h1>Job List</h1>
+      {data.map((job, index) => (
+        <div key={index}>
+          <p>{job.jobTitle}</p>
+          <p>{job.companyName}</p>
+          <p>{job.location}</p>
+          <p>{job.description}</p>
+          <p>{job.requirements}</p>
+        </div>
+      ))}
     </div>
   );
 }
